65f4d06a971e74ce888e7ee17edf6898a8c055d3
Antonio D <tmitetime@gmail.com>
2022-05-02 12:11:16 -0400

    Paypal Button fixed on reload

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,18 @@
 class CartsController < ApplicationController
+  before_action :set_gon, only: [:show]
+  #after_action :refresh_page, only: [:show]
   def show
     @order_items = current_order.order_items
+    #gon.finalPrice = current_order.subtotal
+  end
+
+  private
+
+  def set_gon
     gon.finalPrice = current_order.subtotal
+  end
+  
+  def refresh_page
+    redirect_to request.referrer
   end
 end
