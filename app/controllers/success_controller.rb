9759e48a2be0fcfb666f9e78339081f4e62d04b6
Antonio D <tmitetime@gmail.com>
2022-05-09 16:11:34 -0400

    Changed mail sending time

--- app/controllers/success_controller.rb
+++ app/controllers/success_controller.rb
@@ -1,7 +1,14 @@
 class SuccessController < ApplicationController
+    after_action :remove_cart, only: [:index]
     def index
         @order = current_order
         OrderNotficationEmailMailer.notfication(@order).deliver
+        
+    end
+
+    private
+
+    def remove_cart
         current_order.order_items.delete_all
     end
 end
