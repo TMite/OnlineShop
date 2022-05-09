6572d600fe8460010730df6e0f1868ea8a086755
Antonio D <tmitetime@gmail.com>
2022-05-09 14:43:16 -0400

    Heroku init commit

--- app/controllers/success_controller.rb
+++ app/controllers/success_controller.rb
@@ -1,6 +1,7 @@
 class SuccessController < ApplicationController
     def index
         @order = current_order
-        OrderNotficationEmailMailer.notfication(@order).deliver_later
+        OrderNotficationEmailMailer.notfication(@order).deliver
+        current_order.order_items.delete_all
     end
 end
--- app/mailers/order_notfication_email_mailer.rb
+++ app/mailers/order_notfication_email_mailer.rb
@@ -1,6 +1,7 @@
 class OrderNotficationEmailMailer < ApplicationMailer
     def notfication(order)
         @order = order.order_items
-        mail(to: ENV['gmail_username'], subject: 'Email Test')
+        @order_total = order.subtotal
+        mail(to: ENV['gmail_username'], subject: 'Email Test', content_type: 'text/html')
     end
 end
