474e2940c375e8a809edbe19a622983f26486dfa
Antonio D <tmitetime@gmail.com>
2022-05-09 17:42:40 -0400

    Production host change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,12 +99,12 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { 
-  :host => 'localhost:3000', 
+  :host => 'neesh-foods.herokuapp.com', 
   :protocol => 'http'
   }
   config.action_mailer.smtp_settings = {
   :address => 'smtp.gmail.com',
-  :port => 3000,
+  :port => 587,
   :user_name => ENV['gmail_username'],
   :password => ENV['gmail_password'],
   :authentication => 'plain',
