a112c31f1a63a3e21771e4caa18fb13d7d789cd5
Antonio D <tmitetime@gmail.com>
2022-06-04 16:53:02 -0400

    Added catering page coming soon and moved order info to partial

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   resources :order
   resource :cart, only: [:show]
   resource :social, only: [:show]
-  resources :success
+  resources :success, only: [:index]
+  resources :catering, only: [:index]
 
   root 'shops#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
---
+++ test/controllers/catering_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CateringControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
