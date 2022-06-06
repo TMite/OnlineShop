12c194264c8d749c4ad47be8aa3d17e3324eb4f7
Antonio D <tmitetime@gmail.com>
2022-06-05 22:52:43 -0400

    CSS changes for better viewing on mobile and order validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,10 @@
 class Order < ApplicationRecord
     has_many :order_items
     before_save :set_subtotal
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :address, presence: true
+    validates :date, presence: true
     def subtotal
         order_items.collect{|order_item| order_item.valid? ? order_item.unit_price*order_item.quantity : 0}.sum
     end
