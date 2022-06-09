d167004feebf031d3898019503e8675d320e788d
Antonio D <tmitetime@gmail.com>
2022-06-09 15:46:10 -0400

    Changed some css for easier mobile viewing

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,18 +1,10 @@
 class ShopsController < ApplicationController
   def index
     puts session[:order_id]
-    # if @order.id == nil
-    #   puts "NO ID"
-
-    # end
     @products = Product.all
   end
 
   def show
-    # if @order.id == nil
-    #   puts "No ID"
-    #   puts @order.id
-    # end
     @order_item = current_order.order_items.new
     @product = Product.find(params[:id])
   end
