4c4f0ede1b02161c865961d4ad674524367f8ef5
Antonio D <tmitetime@gmail.com>
2022-06-10 14:31:22 -0400

    Email validation form changes and default values added

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,17 @@
 class Order < ApplicationRecord
     has_many :order_items
     before_save :set_subtotal
+    after_initialize :set_defaults
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP} 
     def subtotal
         order_items.collect{|order_item| order_item.valid? ? order_item.unit_price*order_item.quantity : 0}.sum
+    end
+
+    def set_defaults
+        self.name = "John Doe"
+        self.email = "johndoe@gmail.com"
+        self.address = "1234 Road Dr"
+        self.date = Date.today
     end
 
     private 
